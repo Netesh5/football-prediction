34dfcf29ce37a09f708df35ed5f5354b031f509f
netesh5 <netesh.paudel@gmail.com>
2021-12-09 21:51:57 +0545

    first_commit

---
+++ android/app/src/main/kotlin/com/example/footballprediction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.footballprediction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Football Prediction"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+    );
+  }
+}
